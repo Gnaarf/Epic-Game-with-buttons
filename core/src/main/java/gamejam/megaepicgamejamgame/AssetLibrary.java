80beb2c91e2778735f2672aca3a96da025a9f8d0
Gnaarf <gnaarf@gmx.de>
2025-11-08 21:24:21 +0100

    added textures (index finger, safety lid)

--- core/src/main/java/gamejam/megaepicgamejamgame/AssetLibrary.java
+++ core/src/main/java/gamejam/megaepicgamejamgame/AssetLibrary.java
@@ -11,6 +11,9 @@ public final class AssetLibrary {
     public Texture libgdx_logo;
     public Texture button_down;
     public Texture button_up;
+    public Texture pointing_index_finger;
+    public Texture safety_lid;
+    public Texture safety_lid_transparent;
 
     // Sounds
     public Sound moan_01;
@@ -20,6 +23,9 @@ public final class AssetLibrary {
         button_down = new Texture("button_down.png");
         button_up = new Texture("button_up.png");
         libgdx_logo = new Texture("libgdx.png");
+        pointing_index_finger = new Texture("pointing_index_finger.png");
+        safety_lid = new Texture("safety_lid.png");
+        safety_lid_transparent = new Texture("safety_lid_transparent.png");
 
         moan_01 = Gdx.audio.newSound(Gdx.files.internal("moan_01.mp3"));
         moan_02 = Gdx.audio.newSound(Gdx.files.internal("moan_01.mp3"));
